8cea2fa9d79eaf6754de48ffceebb0b158da629d
Anupriya <51444794+Anupriya11@users.noreply.github.com>
2019-06-07 15:30:23 +0530

    Add files via upload

---
+++ Subscribers/Opportunity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMAzureBusIntegration.Subscribers
+{
+    public class Opportunity 
+    {
+        public Opportunity()
+        {
+        }
+        public void Execute(RemoteExecutionContext executionContext)
+        {
+            
+        }
+    }
+}
