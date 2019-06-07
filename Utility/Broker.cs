01eca8dd24323bea7bb475dbe9da53e25a4bcf6d
Anupriya <51444794+Anupriya11@users.noreply.github.com>
2019-06-07 15:31:15 +0530

    Add files via upload

---
+++ Utility/Broker.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Configuration;
+
+namespace CRMAzureBusIntegration
+{
+    public class Broker
+    {
+        public static void Listener(RemoteExecutionContext context)
+        {
+            try
+            {
+                IEnumerable<Type> classList = GetClasses();
+                if (classList != null)
+                {
+                    foreach (Type cls in classList.Where(x => x.Namespace == ConfigurationManager.AppSettings["Namespace"]))
+                    {
+                        if (cls.Name == context.PrimaryEntityName)
+                        {
+                            Object obj = GetInstance(cls);
+                            if (obj != null)
+                            {
+                                MethodInfo methodInfo = cls.GetMethod("Execute");
+                                if (methodInfo != null)
+                                {
+                                    object[] parametersArray = new object[] { context };
+                                    methodInfo.Invoke(obj, parametersArray);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Inside Listener: Error[" + ex.Message + "]");
+            }
+        }
+        static IEnumerable<Type> GetClasses()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            return asm != null ? asm.GetTypes() : null;
+        }
+        static object GetInstance(Type typeList)
+        {
+            return typeList != null ? Activator.CreateInstance(typeList, false) : null;
+        }
+    }
+}
---
+++ Utility/Common.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Configuration;
+using System.Net;
+
+namespace CRMAzureBusIntegration.Utility
+{
+    public class Common
+    {
+        public IOrganizationService InitializeService()
+        {
+            IOrganizationService service = null;
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                CrmServiceClient serviceClient = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRM"].ConnectionString);
+                service = serviceClient.OrganizationWebProxyClient ?? (IOrganizationService)serviceClient.OrganizationServiceProxy;
+                Entity a = service.Retrieve("account", new Guid("DDA36E51-9944-E911-A825-000D3A1670F6"), new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Inside InitializeService - Exception - " + ex.Message);
+            }
+            return service;
+        }
+    }
+}
